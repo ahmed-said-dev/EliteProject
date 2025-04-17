fcb1de7fe5461de81e70acadc25327b860dc627a
ahmed-said-dev <eng.ahmed.said.97@gmail.com>
2025-04-17 15:25:20 +0200

    `Update backgroundImage in PageBanner component in appointment.js`

--- elite-frontend/pages/appointment.js
+++ elite-frontend/pages/appointment.js
@@ -12,7 +12,7 @@ export default function Appointment() {
     <main>
       <PageBanner 
         title="Book Appointment"
-        backgroundImage="/images/banner/bnr1.webp"
+        backgroundImage="https://images.unsplash.com/photo-1603133872878-684f208fb84b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
       />
       <AppointmentIntro />
       {/* <SectionDivider icon="fas fa-calendar-check" color="#7c58d3" /> */}
