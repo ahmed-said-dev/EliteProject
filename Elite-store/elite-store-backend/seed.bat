f75580dc4d79dccfdc6fee348d7cbeb56f459088
ahmed-said-dev <eng.ahmed.said.97@gmail.com>
2025-08-21 14:23:05 +0300

    URGENT: Fix Swagger UI setup

---
+++ Elite-store/elite-store-backend/seed.bat
@@ -0,0 +1,7 @@
+set "DB_HOST=elite-store-db-do-user-24606323-0.i.db.ondigitalocean.com" && ^
+set "DB_PORT=25060" && ^
+set "DB_USERNAME=doadmin" && ^
+set "DB_PASSWORD=AVNS_Sfg3cMWF_zNOSTFufbo" && ^
+set "DB_NAME=defaultdb" && ^
+set "NODE_ENV=production" && ^
+npm run seed
